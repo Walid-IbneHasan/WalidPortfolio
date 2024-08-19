cca3f8b3f7dfbcbd2b5129bf0fb68a925c82df55
Walid Ibne Hasan <walid.ibne.hasan@gmail.com>
2024-08-19 20:01:09 +0600

    Mobile device isMobile true fixed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -46,6 +46,8 @@ const ComputersCanvas = () => {
 
     // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
+    console.log("Is Mobile:", mediaQuery.matches);
+    handleMediaQueryChange(mediaQuery);
 
     // Remove the listener when the component is unmounted
     return () => {
@@ -55,7 +57,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
