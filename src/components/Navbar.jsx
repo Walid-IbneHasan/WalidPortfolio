a01bd72da15d6df6f4201fe3f79612dc607c7ca4
Walid Ibne Hasan <walid.ibne.hasan@gmail.com>
2025-10-20 23:46:45 +0600

    Logo Resized

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src="/logo.png" alt="logo" className="w-9 h-9 object-contain" />
+          <img src="/logo.png" alt="logo" className="w-12 h-12 lg:w-16 lg:h-16 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Walid &nbsp;
             <span className="sm:block hidden"> </span>
